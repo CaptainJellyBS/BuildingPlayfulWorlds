642bd77ee03c3e8d2b7a3d7b4d960f31861c76a9
Mana <manakouwenberg@gmail.com>
2020-02-13 08:59:34 +0100

    BT-0003 -Finished Video 3

--- BrackeyTutorial/BrackeyTutorial/Assets/Scripts/PlayerMovement.cs
+++ BrackeyTutorial/BrackeyTutorial/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
+    [SerializeField]
+    [Range(0,5000)]
+    float forwardSpeed = 1000f;
+
+    [SerializeField]
+    [Range(0, 5000)]
+    float sideSpeed = 500f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,16 +21,24 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
     }
 
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 1000 * Time.deltaTime);
+        HandleInput();
+        rb.AddForce(0, 0, forwardSpeed * Time.deltaTime);
     }
 
     void HandleInput()
     {
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideSpeed * Time.deltaTime, 0, 0);
+        }
 
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideSpeed * Time.deltaTime, 0, 0);
+        }
     }
 }
