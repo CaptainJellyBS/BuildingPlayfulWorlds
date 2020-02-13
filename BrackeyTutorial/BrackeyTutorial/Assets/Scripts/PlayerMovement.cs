f1a49731cae18798d5e7639935d293f821853f1d
Mana <manakouwenberg@gmail.com>
2020-02-13 08:49:00 +0100

    BT-0002 -Finished Video 2

--- BrackeyTutorial/BrackeyTutorial/Assets/Scripts/PlayerMovement.cs
+++ BrackeyTutorial/BrackeyTutorial/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Debug.Log("HEWWO");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 1000 * Time.deltaTime);
+    }
+
+    void HandleInput()
+    {
+
     }
 }
