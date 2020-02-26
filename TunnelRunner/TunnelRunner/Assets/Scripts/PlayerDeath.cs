ef006356e9466fd7f96415b6186e68af08751e4b
Mana <manakouwenberg@gmail.com>
2020-02-26 10:51:00 +0100

    TR-0005 -Implemented checking for death -Implemented placeholder dying

---
+++ TunnelRunner/TunnelRunner/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0,20)]
+    float DeathDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        CheckForDeath();
+    }
+
+    void CheckForDeath()
+    {
+        if(new Vector3(transform.position.x, transform.position.y, 0).magnitude >= DeathDistance)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Death");
+        Debug.LogError("Dying has not been implemented yet");
+    }
+}
--- TunnelRunner/TunnelRunner/Assets/Scripts/PlayerPhysics.cs
+++ TunnelRunner/TunnelRunner/Assets/Scripts/PlayerPhysics.cs
@@ -29,10 +29,6 @@ public class PlayerPhysics : MonoBehaviour
 
     void RecalculateRotation()
     {
-        //float rotAngle = Vector3.Angle(Vector3.up, new Vector3(transform.position.x, transform.position.y, 0).normalized);
-
-        //transform.RotateAround(transform.position, transform.forward, rotAngle);
-        //(transform.forward, rotAngle);
         Quaternion rotTest = Quaternion.LookRotation(transform.forward, new Vector3(-transform.position.x, -transform.position.y, 0));
         transform.rotation = rotTest;
     }
